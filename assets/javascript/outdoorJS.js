85f23a93a5fe2c30f4579ab85d346864133901ee
Clark Farriss <clarkfarriss@Clarks-MacBook-Pro.local>
2019-01-11 11:31:18 -0500

    api complete

--- assets/javascript/outdoorJS.js
+++ assets/javascript/outdoorJS.js
@@ -1,18 +1,20 @@
-
 //create on click function 
 //take values from location,activity, and distance
 //plug in the values to the api by concatination
 //api takes the data and spits data back out according to what we put in.
 //api puts results onto new page where we want it
 
+var activity;
 
 var submitClick = function () {
     var location = $("#location-input").val().trim();
     console.log(location);
-    var activity = $("#activity").val();
+    activity = $("#activity").val();
     console.log("this is activity chosen: " + activity);
     var radius = $("#distance").val();
     console.log("this is distance chosen: " + radius);
+    var lat;
+    var lng;
 
     var googleQueryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + location + "&inputtype=textquery&fields=name,geometry&key=AIzaSyAZnGA441xuP_Jad74MgbZzz_Yvc6czHCg";
     console.log("this is the goog: " + googleQueryURL);
@@ -22,36 +24,47 @@ var submitClick = function () {
         url: googleQueryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
         var results = response.candidates;
-        var lat = results[0].geometry.location.lat;
+        lat = results[0].geometry.location.lat;
         console.log("this is the lat: " + lat);
-        var lng = results[0].geometry.location.lng;
+        lng = results[0].geometry.location.lng;
         console.log("this is the lng: " + lng);
 
+        if (activity === "walking") {
+            var hikingAPI = "https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lng + "&maxDistance=" + radius + "&key=200406991-51cef123aaf15cad4674f56c0e8aa4f0"
+            console.log("This is hiking: " + hikingAPI);
+            $.ajax({
+                url: hikingAPI,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+            });
+        }
+
+        else if (activity === "biking") {
+            var bikingAPI = "https://www.mtbproject.com/data/get-trails?lat=" + lat + "&lon=" + lng + "&maxDistance=" + radius + "&key=200406991-51cef123aaf15cad4674f56c0e8aa4f0"
+            console.log("this is the mountain biking: " + bikingAPI);
+            $.ajax({
+                url: bikingAPI ,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+            });
+    
+        };
+
+
+
     });
+
+
+
+
+
 }
 
+    
 
 
-//     if (activity === "walking") {
-//         var hikingAPI = "https://www.hikingproject.com/data/get-trails?" + //latlong + "&maxDistance=" + radius + "&key=200406991-51cef123aaf15cad4674f56c0e8aa4f0"
-//         $.ajax({
-//             url: ,
-//             method: "GET"
-//         });
-
-//     };
-
-//     else if (activity === "biking") {
-//         var bikingAPI = 
-//         $.ajax({
-//             url: ,
-//             method: "GET"
-//         });
-
-//     };
-
-// }
 
 $("#submitButton").on("click", submitClick);
